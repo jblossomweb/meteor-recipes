4acabb6bd17b655c8f918c1bebd423a783f6a388
John Blossom <jblossom@medianewsgroup.com>
2016-10-03 12:56:29 -0600

    template-level subscribe

--- client/recipes/Recipes.js
+++ client/recipes/Recipes.js
@@ -1,9 +1,15 @@
-Meteor.subscribe('recipes');
-
-// console.log(Meteor.settings.public.ga.account);
+// tie recipes to template instead of route (performs better)
+Template.Recipes.onCreated(function(){
+  var self = this;
+  self.autorun(function(){
+    self.subscribe('recipes');
+  });
+});
 
 Template.Recipes.helpers({
   recipes: function() {
     return Recipes.find({});
   }
 });
+
+// console.log(Meteor.settings.public.ga.account);
