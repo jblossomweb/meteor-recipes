d6b636bb8ab6a95937a4d968c766d1656feedb93
John Blossom <jblossom@jblossom-denver.local>
2016-10-03 11:28:45 -0600

    create, layouts, schema, quickform

---
+++ client/main.js
@@ -0,0 +1,6 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import { Recipes } from '/collections/Recipes.js';
+
+// import '/client/main.html';
---
+++ collections/Recipes.js
@@ -0,0 +1,34 @@
+Recipes = new Meteor.Collection('recipes');
+
+Recipes.attachSchema(new SimpleSchema({
+  name: {
+    type: String,
+    label: "Name"
+  },
+  description: {
+    type: String,
+    label: "Description"
+  },
+  author: {
+    type: String,
+    label: "Author",
+    autoValue: function() {
+      return this.userId;
+    },
+    autoform: {
+      type: "hidden"
+    }
+  },
+  createdAt: {
+    type: Date,
+    label: "Created At",
+    autoValue: function() {
+      return new Date();
+    },
+    autoform: {
+      type: "hidden"
+    }
+  }
+}));
+
+export default Recipes;
