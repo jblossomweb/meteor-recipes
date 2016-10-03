f71809dcca280b40f3e4948a1301837f7da21d6f
John Blossom <jblossom@medianewsgroup.com>
2016-10-03 12:47:25 -0600

    iterate over recipes

--- client/recipes/Recipes.js
+++ client/recipes/Recipes.js
@@ -1,3 +1,9 @@
 Meteor.subscribe('recipes');
 
-console.log(Meteor.settings.public.ga.account);
+// console.log(Meteor.settings.public.ga.account);
+
+Template.Recipes.helpers({
+  recipes: function() {
+    return Recipes.find({});
+  }
+});
